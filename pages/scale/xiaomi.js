c437677653bb13d431fdb6d7d3e564334ce9ce05
Lukasz Swiderski <lukasz@swiderski.xyz>
2023-04-23 15:09:18 +0200

    copied code from google chrome web bluetooth scan sample

--- pages/scale/xiaomi.js
+++ pages/scale/xiaomi.js
@@ -1,3 +1,39 @@
+import onScan from '../../services/webscan'
 export default function Xiaomi() {
-    return <h1>Xiaomi Page</h1>
+
+    async function handleScan() {
+        await onScan();
+
+    }
+
+    return (
+        <div>
+            <h1>Xiaomi Page</h1>
+            <div>
+            </div>
+            <div>
+
+                <ul>
+                    <li>https://googlechrome.github.io/samples/web-bluetooth/scan.html</li>
+                    <li> https://github.com/GoogleChrome/samples/tree/gh-pages/web-bluetooth</li></ul>
+
+
+                <p>This sample illustrates basic use of the Web Bluetooth Scanning API to
+                    report advertising packets from nearby Bluetooth Low Energy Devices.</p>
+
+                <p>Note: Scanning is still under development. You must be using Chrome 79+
+                    with the <code>chrome://flags/#enable-experimental-web-platform-features</code>&nbsp;
+                    flag enabled.</p>
+            </div>
+
+            <div>
+                <label for="allAdvertisements">All Advertisements</label>
+                <input id="allAdvertisements" type="checkbox" />
+                <input id="name" type="text" size="17" placeholder="Device Name" />
+                <input id="namePrefix" type="text" size="17" placeholder="Device Name Prefix" />
+                <button onClick={handleScan}>Scan for Bluetooth Advertisements</button>
+            </div>
+
+
+        </div>)
 }
---
+++ services/webscan.js
@@ -0,0 +1,69 @@
+export default async function onScan() {
+    let filters = [];
+
+    let filterName = document.querySelector('#name').value;
+    if (filterName) {
+        filters.push({ name: filterName });
+    }
+
+    let filterNamePrefix = document.querySelector('#namePrefix').value;
+    if (filterNamePrefix) {
+        filters.push({ namePrefix: filterNamePrefix });
+    }
+
+    let options = {};
+    if (document.querySelector('#allAdvertisements').checked) {
+        options.acceptAllAdvertisements = true;
+    } else {
+        options.filters = filters;
+    }
+
+    try {
+        console.log('Requesting Bluetooth Scan with options: ' + JSON.stringify(options));
+        const scan = await navigator.bluetooth.requestLEScan(options);
+
+        console.log('Scan started with:');
+        console.log(' acceptAllAdvertisements: ' + scan.acceptAllAdvertisements);
+        console.log(' active: ' + scan.active);
+        console.log(' keepRepeatedDevices: ' + scan.keepRepeatedDevices);
+        console.log(' filters: ' + JSON.stringify(scan.filters));
+
+        navigator.bluetooth.addEventListener('advertisementreceived', event => {
+            console.log('Advertisement received.');
+            console.log('  Device Name: ' + event.device.name);
+            console.log('  Device ID: ' + event.device.id);
+            console.log('  RSSI: ' + event.rssi);
+            console.log('  TX Power: ' + event.txPower);
+            console.log('  UUIDs: ' + event.uuids);
+            event.manufacturerData.forEach((valueDataView, key) => {
+                logDataView('Manufacturer', key, valueDataView);
+            });
+            event.serviceData.forEach((valueDataView, key) => {
+                logDataView('Service', key, valueDataView);
+            });
+        });
+        debugger;
+        setTimeout(stopScan, 10000);
+        function stopScan() {
+            console.log('Stopping scan...');
+            scan.stop();
+            console.log('Stopped.  scan.active = ' + scan.active);
+        }
+    } catch (error) {
+        debugger;
+        console.log('Argh! ' + error);
+    }
+}
+
+/* Utils */
+
+const logDataView = (labelOfDataSource, key, valueDataView) => {
+    const hexString = [...new Uint8Array(valueDataView.buffer)].map(b => {
+        return b.toString(16).padStart(2, '0');
+    }).join(' ');
+    const textDecoder = new TextDecoder('ascii');
+    const asciiString = textDecoder.decode(valueDataView.buffer);
+    log(`  ${labelOfDataSource} Data: ` + key +
+        '\n    (Hex) ' + hexString +
+        '\n    (ASCII) ' + asciiString);
+};
