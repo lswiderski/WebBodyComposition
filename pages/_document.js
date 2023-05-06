7df565a2c2dc46fb1857ed0428adfe5323d31cc3
Lukasz Swiderski <lukasz@swiderski.xyz>
2023-05-06 21:29:23 +0200

    Removed Ads

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          async
-          src={'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1938975042085430'}
-          strategy="lazyOnload"
-          crossOrigin="anonymous"
-        />
       </Head>
       <body>
         <Main />
