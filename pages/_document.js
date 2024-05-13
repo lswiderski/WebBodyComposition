33ff60dde9c828d6d9beb6b353cd6cb6e2675c26
Lukasz Swiderski <lukasz@swiderski.xyz>
2024-05-13 19:41:59 +0200

    Adsense verification

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1938975042085430"
+          crossorigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
